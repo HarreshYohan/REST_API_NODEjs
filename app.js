7ac4b844c26b35d967aa601f5e64cb17d54b2972
HarreshYohan <baptistyohan08@gmail.com>
2020-07-10 14:27:18 +0530

    "small changes"

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 const mongoose = require('mongoose');
-require('dotenv/config');
+require('dotenv/config'); 
 
 //Import route
 const postsRoute = require('./routes/posts');
