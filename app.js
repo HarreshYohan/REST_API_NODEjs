113d83ba69adda06f2b17151fe83307d197c472d
HarreshYohan <baptistyohan08@gmail.com>
2020-07-10 14:24:14 +0530

    initial commit 10072020

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config');
+
+//Import route
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
+
+
+
+//ROUTES
+app.get('/',(req,res)=> {
+    res.send('we are home')
+});
+
+
+
+
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECTION,{useNewUrlParser:true}, ()=>
+console.log('Connected to DB')
+ );
+
+
+//How to we start Listen to the server
+
+app.listen(3000);
